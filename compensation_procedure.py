321a26ceaca00b6e6b81388e98500f6c5eec39cd
vgousyleblanc <51218269+vgousyleblanc@users.noreply.github.com>
2021-01-24 14:48:57 -0800

    Add files via upload

--- compensation_procedure.py
+++ compensation_procedure.py
@@ -29,7 +29,7 @@ print(Am)
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 client = mph.Client()
 model = client.load('magnetic_field_compensation_geo_extended_2.mph')
-
+#%%
 
 model.parameter('z_1_p', '76[cm]')
 model.parameter('z_2_p', '-76[cm]')
@@ -40,14 +40,14 @@ model.parameter('z_2_p', '-76[cm]')
 #model.parameter('y_1_p', '50[cm]')
 #model.parameter('y_2_p', '50[cm]')
 
-model.parameter('V_z1', '0[V]')
-model.parameter('V_z2', '0[V]')
+model.parameter('V_z1', '-5.02[V]')
+model.parameter('V_z2', '-8.29[V]')
 
-model.parameter('V_x12', '0[V]')
-model.parameter('V_x22', '0[V]')
+model.parameter('V_x12', '-1.4[V]')
+model.parameter('V_x22', '-1.382[V]')
 
-model.parameter('V_y1', '0[V]')
-model.parameter('V_y2', '0[V]')
+model.parameter('V_y1', '-1.172[V]')
+model.parameter('V_y2', '-1.20[V]')
 
 """
 model.parameter('V_x12', '2.47[V]')
@@ -58,7 +58,6 @@ model.parameter('V_y2', '-2.16[V]')
 """
 #%%
 model.mesh()
-
 
 start = time.time()
 scan='v'
@@ -92,8 +91,8 @@ if scan=='v' and dim=='x':
 if scan=='v' and dim=='z':
     print('Starting the voltage scan in the '+str(dim)+'  dimension')
     model.solve('Z_sweep')
-    file_name='voltage_scan_z_vf.txt'
-    #model.export('Table 1',file_name)
+    file_name='z_table.txt'
+    model.export('Z_table')
     v0,v1,mx,my,mz=np.loadtxt(file_name,unpack=True,skiprows=5)
     fitm,fitb=optimize(v0,v1,mz,dim)
     computation_time = time.time()-start
@@ -117,16 +116,17 @@ if scan=='s':
                 file_name='spatial_scan_'+str(dim)+str(v0[i])+'.txt'
                 model.export('Full_compensation',file_name)
                 x,y,z,mx,my,mz=np.loadtxt('spatial_scan_'+str(dim)+str(v0[i])+'.txt',unpack=True,skiprows=9)
-                x=x/100
-                y=y/100
-                z=z/100
+                spatial_scan(x,y,z,mx,my,mz)
+                #x=x/100
+                #y=y/100
+                #z=z/100
                 k[i]=differential_fit(x,y,z,mx,my,mz,dim)
-                
+            plt.plot(v1,k,".")   
             slope=linregress(v0,k)
             coil1=-slope.intercept/slope.slope
             coil2=fitm*coil1+fitb
-            print('Tension in coil 1',coil1)
-            print('Tension in coil 2',coil2)
+            print('Tension in coil 3',coil1)
+            print('Tension in coil 4',coil2)
             model.parameter('V_x12', str(coil1)+'[V]')
             model.parameter('V_x22', str(coil2)+'[V]')  
             model.solve('Study 1')
@@ -148,16 +148,18 @@ if scan=='s':
                 file_name='spatial_scan_'+str(dim)+str(v0[i])+'.txt'
                 model.export('Full_compensation',file_name)
                 x,y,z,mx,my,mz=np.loadtxt('spatial_scan_'+str(dim)+str(v0[i])+'.txt',unpack=True,skiprows=9)
-                x=x/100
-                y=y/100
-                z=z/100
+                spatial_scan(x,y,z,mx,my,mz)
+                #x=x/100
+                #y=y/100
+                #z=z/100
             #dim="x"
                 k[i]=differential_fit(x,y,z,mx,my,mz,dim)
+            plt.plot(v1,k,".")
             slope=linregress(v0,k)
             coil1=-slope.intercept/slope.slope
             coil2=fitm*coil1+fitb
-            print('Tension in coil 3',coil1)
-            print('Tension in coil 4',coil2)
+            print('Tension in coil 5',coil1)
+            print('Tension in coil 6',coil2)
             model.parameter('V_y1', str(coil1)+'[V]')
             model.parameter('V_y2', str(coil2)+'[V]')  
             model.solve('Study 1')
@@ -167,8 +169,9 @@ if scan=='s':
             spatial_scan(x,y,z,mx,my,mz)
             end = time.time()
         if dim=='z':
-            v0=np.array([-3,-6,-9])   
-            v1=np.array([fitm*v0[0]+fitb,fitm*v0[1]+fitb,fitm*v0[2]+fitb])
+            v1=np.array([-3,-6,-9])   
+            v0=np.array([fitm*v1[0]+fitb,fitm*v1[1]+fitb,fitm*v1[2]+fitb])
+            print(v0,v1)
             k=np.zeros(len(v0))
             for i in range(0,len(v0)):
                 model.parameter('V_z1', str(v0[i])+'[V]')
@@ -178,13 +181,18 @@ if scan=='s':
                 file_name='spatial_scan_'+str(dim)+str(v0[i])+'.txt'
                 model.export('Full_compensation',file_name)
                 x,y,z,mx,my,mz=np.loadtxt('spatial_scan_'+str(dim)+str(v0[i])+'.txt',unpack=True,skiprows=9)
+                spatial_scan(x,y,z,mx,my,mz)
+                #x=x*100
+                #y=y*100
+                #z=z*100
+                #print(x[0],y[0],z[0])
                 k[i]=differential_fit(x,y,z,mx,my,mz,dim)
-           
+            plt.plot(v1,k,".")
             slope=linregress(v0,k)
             coil1=-slope.intercept/slope.slope
             coil2=fitm*coil1+fitb
-            print('Tension in coil 5',coil1)
-            print('Tension in coil 6',coil2)
+            print('Tension in coil 1',coil1)
+            print('Tension in coil 2',coil2)
             
             model.parameter('V_z1', str(coil1)+'[V]')
             model.parameter('V_z2', str(coil2)+'[V]')  
